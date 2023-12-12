2da98e6e85d8d50faa8659d7faa4e35a10c33183
Chris Wiederspan <chwieder@microsoft.com>
2023-12-12 00:43:23 +0000

    Initial project files added

---
+++ MyConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace MyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking settings...");
+            string connectionString = Environment.GetEnvironmentVariable("DB_CONN_STR");
+            if (string.IsNullOrEmpty(connectionString) == true) {
+                Console.WriteLine("You must set an ENV named DB_CONN_STR with your connection string!");
+                return;
+            }
+            else {
+                Console.WriteLine("Connection string found.");
+            }
+
+            string query = "SELECT OrderID, CustomerID FROM dbo.Orders;";
+
+            Console.WriteLine("Running a query against Orders database table...");
+
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader()) {
+
+                    while (reader.Read()) {
+                        Console.WriteLine(String.Format("{0}, {1}", reader[0], reader[1]));
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# docker-tlstesting-20231212
+# TLS Testing
+
 A quick repo test to see if a Docker container using .NET can access SQL Server
+
+## Setting the DB Connection String
+
+```bash
+
+export DB_CONN_STR="YOUR_CONN_STR"
+
+```
